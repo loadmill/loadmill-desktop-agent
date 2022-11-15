c3d8350399c4911d7623e0dea3c540d0ffd902a6
Gilad Gur Andelman <gilad@loadmill.com>
2022-11-15 10:44:24 +0200

    0.0.6

--- src/index.ts
+++ src/index.ts
@@ -48,6 +48,7 @@ app.on(ACTIVATE, () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
+    console.log('creating window...');
     createWindow();
   }
 });
